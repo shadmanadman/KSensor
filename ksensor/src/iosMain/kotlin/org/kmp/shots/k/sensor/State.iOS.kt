26f55a71f823cd07e0db234e8573b5524e3517f0
shadmanadman <adman.shadman@gmail.com>
2025-11-11 20:14:12 +0330

    Impl location&connectivity monitoring

--- ksensor/src/iosMain/kotlin/org/kmp/shots/k/sensor/State.iOS.kt
+++ ksensor/src/iosMain/kotlin/org/kmp/shots/k/sensor/State.iOS.kt
@@ -7,33 +7,25 @@ import org.kmp.shots.k.sensor.StateUpdate.Data
 import org.kmp.shots.k.sensor.StateUpdate.Error
 import platform.Foundation.NSNotificationCenter
 import platform.Foundation.NSOperationQueue
-import platform.Network.nw_interface_type_cellular
-import platform.Network.nw_interface_type_wifi
-import platform.Network.nw_path_get_status
-import platform.Network.nw_path_monitor_cancel
 import platform.Network.nw_path_monitor_create
-import platform.Network.nw_path_monitor_set_queue
-import platform.Network.nw_path_monitor_set_update_handler
-import platform.Network.nw_path_monitor_start
-import platform.Network.nw_path_status_satisfied
-import platform.Network.nw_path_uses_interface_type
 import platform.UIKit.UIApplicationDidEnterBackgroundNotification
 import platform.UIKit.UIApplicationWillEnterForegroundNotification
 import platform.darwin.NSObject
-import platform.darwin.dispatch_queue_create
 
 internal actual class StateHandler : StateController {
     private var foregroundObserver: NSObject? = null
     private var backgroundObserver: NSObject? = null
     private val monitor = nw_path_monitor_create()
-    private val queue = dispatch_queue_create("NetworkMonitor", null)
 
+    private lateinit var locationProviderReceiver: LocationProviderReceiver
+    private val connectivityMonitor = ConnectivityMonitor
     actual override fun addObserver(types: List<StateType>): Flow<StateUpdate> = callbackFlow {
         types.forEach { stateType ->
             when (stateType) {
                 StateType.SCREEN -> trySend(Error(exception = Exception("iOS dos not have a convince way to check screen state")))
                 StateType.APP_VISIBILITY -> observerAppVisibility { trySend(it).isSuccess }
                 StateType.CONNECTIVITY, StateType.ACTIVE_NETWORK -> observeConnectivity { trySend(it).isSuccess }
+                StateType.LOCATION -> observeLocation { trySend(it).isSuccess }
             }.also {
                 println("Observer added for $stateType on iOS")
             }
@@ -53,7 +45,11 @@ internal actual class StateHandler : StateController {
                     }
                 }
 
-                StateType.CONNECTIVITY, StateType.ACTIVE_NETWORK -> nw_path_monitor_cancel(monitor)
+                StateType.CONNECTIVITY, StateType.ACTIVE_NETWORK -> connectivityMonitor.unregister(
+                    monitor
+                )
+
+                StateType.LOCATION -> locationProviderReceiver.dispose()
             }.also {
                 println("Observer removed for $stateType on iOS")
             }
@@ -66,40 +62,35 @@ internal actual class StateHandler : StateController {
         onPermissionStatus: (PermissionStatus) -> Unit
     ) = Unit
 
-    private fun observeConnectivity(onData: (StateUpdate) -> Boolean) {
-        nw_path_monitor_set_update_handler(monitor) { path ->
-            val status = nw_path_get_status(path)
-            val isConnected = status == nw_path_status_satisfied
+    private fun observeLocation(onData: (StateUpdate) -> Boolean) {
+        locationProviderReceiver = LocationProviderReceiver {
             onData(
                 Data(
-                    type = StateType.CONNECTIVITY,
-                    StateData.ConnectivityStatus(
-                        isConnected = isConnected
-                    ),
-                    PlatformType.Android
-                )
-            )
-
-            val activeNetwork = if (nw_path_uses_interface_type(path, nw_interface_type_wifi))
-                StateData.CurrentActiveNetwork.ActiveNetwork.WIFI
-            else if (nw_path_uses_interface_type(path, nw_interface_type_cellular))
-                StateData.CurrentActiveNetwork.ActiveNetwork.CELLULAR
-            else
-                StateData.CurrentActiveNetwork.ActiveNetwork.NONE
-
-            onData(
-                Data(
-                    type = StateType.ACTIVE_NETWORK,
-                    StateData.CurrentActiveNetwork(
-                        activeNetwork = activeNetwork
-                    ),
-                    PlatformType.Android
+                    type = StateType.LOCATION, data = StateData.LocationStatus(it),
+                    PlatformType.iOS
                 )
             )
         }
+    }
 
-        nw_path_monitor_set_queue(monitor, queue)
-        nw_path_monitor_start(monitor)
+    private fun observeConnectivity(onData: (StateUpdate) -> Boolean) {
+        connectivityMonitor.register(monitor, isConnected = {
+            onData(
+                StateUpdate.Data(
+                    type = StateType.CONNECTIVITY,
+                    data = StateData.ConnectivityStatus(isConnected = it),
+                    platformType = PlatformType.iOS
+                )
+            )
+        }, currentActiveNetwork = {
+            onData(
+                StateUpdate.Data(
+                    type = StateType.ACTIVE_NETWORK,
+                    data = StateData.CurrentActiveNetwork(it),
+                    platformType = PlatformType.iOS
+                )
+            )
+        })
     }
 
     private fun observerAppVisibility(onData: (StateUpdate) -> Boolean) {
