5cc43094e12d61993f7d835e797a51edc4102798
shadmanadman <adman.shadman@gmail.com>
2026-02-11 13:47:05 +0330

    Change namespace

---
+++ ksensor/src/commonMain/kotlin/org/kmp/ksensor/KSensor.kt
@@ -0,0 +1,31 @@
+package org.kmp.ksensor
+
+import androidx.compose.runtime.Composable
+import permission.PermissionStatus
+import permission.PermissionType
+import sensor.SensorController
+import sensor.SensorTimeInterval
+import sensor.SensorType
+import sensor.createController
+
+object KSensor : SensorController {
+    private val sensorController = createController()
+
+    override fun registerSensors(
+        types: List<SensorType>,
+        locationIntervalMillis: SensorTimeInterval
+    ) = sensorController.registerSensors(types, locationIntervalMillis)
+
+    override fun unregisterSensors(types: List<SensorType>) =
+        sensorController.unregisterSensors(types)
+
+    @Composable
+    override fun AskPermission(
+        permissionType: PermissionType,
+        permissionStatus: (PermissionStatus) -> Unit
+    ) = sensorController.AskPermission(permissionType, permissionStatus)
+
+
+    @Composable
+    override fun OpenSettingsForPermission() = sensorController.OpenSettingsForPermission()
+}
